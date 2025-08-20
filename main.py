5ee52c4c67eda2cba21e9089ecb146d5787db9d3
fns12 <fareenanoorsatti@gmail.com>
2025-08-20 16:51:55 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 import os
